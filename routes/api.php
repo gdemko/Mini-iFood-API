c6e01025aece07de84f611ce2c8143dc5f8f6d27
Maicon Cerutti <maicon.cerutti@facedigital.com.br>
2020-09-10 22:57:32 -0300

    add

--- routes/api.php
+++ routes/api.php
@@ -23,4 +23,6 @@ Route::post('login', 'UserController@authenticate');
 
 Route::group(['middleware' => ['jwt.verify']], function() {
     Route::get('user', 'UserController@getAuthenticatedUser');
+
+    
 });
